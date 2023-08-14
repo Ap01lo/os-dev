a4668c898b182c3be248d85016179d1a750815c1
Ap01lo <apollo.xlZhao@gmail.com>
2023-08-14 18:35:28 +0800

    docs: Add chapter 3-4-2

--- code/boot_sect.asm
+++ code/boot_sect.asm
@@ -6,16 +6,18 @@ mov ah, 0x0e
 ;mov al, [the_secret]
 ;int 0x10
 
-mov bx, 0x7c00
-add bx, the_secret 
-mov al, [bx]
-int 0x10
-
-;mov al, [0x7c1e]
+;mov bx, 0x7c00
+;add bx, the_secret 
+;mov al, [bx]
 ;int 0x10
+
+mov al, [0x7c09]
+int 0x10
 
 the_secret:
 	db "X"
+
+jmp $
 
 times 510-($-$$) db 0
 
