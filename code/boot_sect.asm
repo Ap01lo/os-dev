b48d4f92aaa6a7bdf472473fadc9f1b8a995adb8
Ap01lo <apollo.xlZhao@gmail.com>
2023-08-14 17:47:56 +0800

    code: Find the byte

--- code/boot_sect.asm
+++ code/boot_sect.asm
@@ -1,19 +1,21 @@
 mov ah, 0x0e
 
-mov al, 'H'
+;mov al, the_secret
+;int 0x10
+
+;mov al, [the_secret]
+;int 0x10
+
+mov bx, 0x7c00
+add bx, the_secret 
+mov al, [bx]
 int 0x10
 
-mov al, 'e'
-int 0x10
+;mov al, [0x7c1e]
+;int 0x10
 
-mov al, 'l'
-int 0x10
-
-mov al, 'l'
-int 0x10
-
-mov al, 'o'
-int 0x10
+the_secret:
+	db "X"
 
 times 510-($-$$) db 0
 
