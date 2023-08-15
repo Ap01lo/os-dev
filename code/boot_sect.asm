e46ac9b6ee52adfa15beb4a6616ebe324cbbf20f
Ap01lo <apollo.xlZhao@gmail.com>
2023-08-15 10:18:45 +0800

    code: Update stack usage

--- code/boot_sect.asm
+++ code/boot_sect.asm
@@ -1,22 +1,23 @@
-org 0x7c00
 mov ah, 0x0e
 
-;mov al, the_secret
-;int 0x10
+mov bp, 0x8000
+mov sp, bp
 
-;mov al, [the_secret]
-;int 0x10
+push 'AB'
 
-;mov bx, 0x7c00
-;add bx, the_secret 
-;mov al, [bx]
-;int 0x10
-
-mov al, [the_secret]
+mov al, [0x7ffe]
 int 0x10
 
-the_secret:
-	db "X"
+mov al, [0x7fff]
+int 0x10
+
+
+pop bx
+mov al, bl
+int 0x10
+
+mov al, bh
+int 0x10
 
 jmp $
 
