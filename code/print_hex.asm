13db93fa782e01c5d3a10c80981e3c922d1772ea
Ap01lo <apollo.xlZhao@gmail.com>
2023-08-17 19:21:32 +0800

    code: Add print_hex.asm to convert hex address to ascii and then print

---
+++ code/print_hex.asm
@@ -0,0 +1,37 @@
+print_hex:
+pusha
+mov ax, dx
+mov bx, HEX
+add bx, 5
+mov cx, 0
+
+convert:
+    mov dx,ax
+    and dx, 0x000f
+    call bin_to_ascii
+    mov byte [bx], dl
+    dec bx
+    shr ax, 4
+    add cx, 1
+    cmp ax, 0
+    jne convert
+    
+jmp print_h
+
+bin_to_ascii:
+    cmp dl, 0x0a
+    jl to_num
+    jmp to_lower
+
+to_num:
+    add dl, 48
+    ret
+to_lower:
+    add dl, 87 
+    ret
+
+print_h:
+    mov bx, HEX
+    call print_string
+popa
+ret
