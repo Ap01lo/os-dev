997c950f2796f7fdfd65cfd76c72d938c189deb6
Ap01lo <apollo.xlZhao@gmail.com>
2023-08-18 22:57:39 +0800

    code: Upload boot_sect to test disk_load

--- code/boot_sect.asm
+++ code/boot_sect.asm
@@ -1,16 +1,35 @@
 org 0x7c00
 
-mov dx, 0x1f2d
-mov bx, HEX
+mov [BOOT_DRIVE], dl
+; 将栈区设置远离代码区
+mov bp, 0x8000
+mov sp, bp
+
+; 读取磁盘数据至 [0x0000:0x9000]
+mov bx, 0x9000
+mov dh, 5
+mov dl, [BOOT_DRIVE]
+call disk_load
+
+; 查看读取结果 使用print_hex查看内容
+mov dx, [0x9000]
 call print_hex
 
+mov dx, [0x9000+512]
+call print_hex
 jmp $
 
-%include "print_string.asm"
-%include "print_hex.asm"
+BOOT_DRIVE:
+    db 0x00
 
-HEX:
-dw "0x0000",0
+%include "print_hex.asm"
+%include "print_string.asm"
+%include "disk_load.asm"
 
 times 510-($-$$) db 0
 dw 0xaa55
+
+; set following 2 sectors
+times 256 dw 0xdada                 ; sector2
+times 256 dw 0x1234                 ; sector3
+
