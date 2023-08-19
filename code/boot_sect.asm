f5483f212606c809cf974130849fcd3932f248d8
Ap01lo <apollo.xlZhao@gmail.com>
2023-08-20 00:21:12 +0800

    code: Update boot_sect tp convert from rm to pm

--- code/boot_sect.asm
+++ code/boot_sect.asm
@@ -1,38 +1,46 @@
+; 一个进入32-bit保护模式的引导程序
 org 0x7c00
 
-mov [BOOT_DRIVE], dl
-; 将栈区设置远离代码区
-mov bp, 0x8000
+; 设置栈
+mov bp, 0x9000
 mov sp, bp
 
-; 读取磁盘数据至 [0x0000:0x9000]
-mov bx, 0x9000
-mov dh, 0x02
-mov dl, [BOOT_DRIVE]
-; call print_regs
-call disk_load
+mov bx, MSG_REAL_MODE
+call print_string
 
-; 查看读取结果 使用print_hex查看内容
-mov dx, [0x9000]
-call print_hex
-
-mov dx, [0x9000+512]
-call print_hex
+; 转换到32-bit保护模式,而且永远不再回来了
+call switch_to_pm
 
 jmp $
 
-BOOT_DRIVE:
-    db 0x00
-
-%include "print_hex.asm"
 %include "print_string.asm"
-%include "disk_load.asm"
-%include "debug.asm"
+%include "gdt.asm"
+%include "print_string_pm.asm"
+%include "switch_to_pm.asm"
+
+; 我们转换模式后跳转到这里，并做一些初始化工作
+
+BEGIN_PM:
+[bits 32]
+
+; mov ah, 0x0e
+; mov al, 'p'
+; int 0x10
+
+mov ebx, MSG_PROT_MODE
+; 调用32-bit的输出程序
+call print_string_pm
+
+a:
+mov eax,0xaaaaaaaa
+mov ebx,0xbbbbbbbb
+mov ecx,0xcccccccc
+mov edx,0xdddddddd
+jmp a		; 一致挂载
+	
+; 全局变量
+MSG_REAL_MODE db "Started in 16-bit Real Mode",0x0a,0x0d,0
+MSG_PROT_MODE db "Successfully landed in 32 Protected Mode"0x0a,0x0d,0
 
 times 510-($-$$) db 0
 dw 0xaa55
-
-; set following 2 sectors
-times 256 dw 0xdada                 ; sector2
-times 256 dw 0xface                 ; sector3
-
